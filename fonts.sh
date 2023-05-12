e4301e5fb4249e3e96eb8aa7561083343c64f394
username12 <91358234+BHUTUUU@users.noreply.github.com>
2023-05-11 21:22:13 -0600

    Add files via upload

---
+++ fonts.sh
@@ -0,0 +1,6 @@
+mkdir -p $HOME/Downloads/nerdfonts/
+cd $HOME/Downloads/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.1/CascadiaCode.zip
+unzip '*.zip' -d $HOME/Downloads/nerdfonts/
+rm -rf *.zip
+sudo cp -R $HOME/Downloads/nerdfonts/ /usr/share/fonts/
